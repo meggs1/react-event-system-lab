255c419e0e7891d1bb904e9c0013111c80acf3b1
megan <megan.schertz@gmail.com>
2021-09-20 11:36:25 -0500

    commit tree 8dc5e92830ec62d416b22f17e30f7eee2e8d89ea

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+import { Component } from "react";
+
+class Keypad extends Component {
+
+    render() {
+        return (
+            <input type="password" onKeyUp=""></input>
+        )
+    }
+}
+
+export default Keypad
